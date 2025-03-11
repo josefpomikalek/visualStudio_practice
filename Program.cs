9fc350e7ccede20bddf4de1cedcded47fddc8d26
josefpomikalek <josef_pomikalek@yahoo.com>
2025-03-11 15:31:41 +0100

    Use of simple conditions.

--- Program.cs
+++ Program.cs
@@ -4,7 +4,28 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            const string testLoginAdmin = "admin";
+            const string testPasswordAdmin = "csharp";
+
+            Console.WriteLine("Vítejte v programu na procvičování podmínek");
+            Console.Write("Zadejte uživatelské jméno: ");
+            string login = Console.ReadLine();
+            Console.Write("Zadejte heslo: ");
+            string password = Console.ReadLine();
+
+            if (login == testLoginAdmin && password == testPasswordAdmin)
+            {
+                Console.WriteLine("Jste přihlášeni jako administrátor.");
+            }
+            else if (login == testLoginAdmin && password != testPasswordAdmin)
+            {
+                Console.WriteLine("Zadali jste neplatné přihlašovací údaje administrátora.");
+            }
+            else
+            {
+                Console.WriteLine("Jste přihlášeni jako běžný uživatel.");
+            }
+
         }
     }
 }
