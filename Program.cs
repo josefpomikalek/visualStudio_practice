49dc4002ae0b03f53604bee9b26920716959321e
josefpomikalek <josef_pomikalek@yahoo.com>
2025-03-12 15:53:37 +0100

    Add cycle 'while' - calculator.

--- Program.cs
+++ Program.cs
@@ -26,6 +26,8 @@
             //    Console.WriteLine("Jste přihlášeni jako běžný uživatel.");
             //}
 
+            //************************************************************************
+            //************************************************************************
 
 
             //Console.WriteLine("*** Vítejte v naší restauraci! ***");
@@ -75,85 +77,117 @@
             //        break;
             //}
 
-            string telefonKlient;
-            string telefonNeklient;
-            string telefonProdukt;
+            //************************************************************************
+            //************************************************************************
 
-            Console.WriteLine("Vítejte v našem informačním rozcestníku.");
-            Console.WriteLine();
-            Console.Write("Jste klientem Komerční banky? (ano / ne): ");
-            string jeKlientem = Console.ReadLine();
+            //string telefonKlient;
+            //string telefonNeklient;
+            //string telefonProdukt;
 
-            if ((jeKlientem != "ano") && (jeKlientem != "ne"))
+            //Console.WriteLine("Vítejte v našem informačním rozcestníku.");
+            //Console.WriteLine();
+            //Console.Write("Jste klientem Komerční banky? (ano / ne): ");
+            //string jeKlientem = Console.ReadLine();
+
+            //if ((jeKlientem != "ano") && (jeKlientem != "ne"))
+            //{
+            //    Console.WriteLine();
+            //    Console.WriteLine("Vaše volba nebyla rozpoznána.");
+            //}
+            //else
+            //{
+            //    Console.WriteLine();
+            //    Console.WriteLine("S jakým produktem potřebujete poradit?");
+            //    Console.WriteLine();
+            //    Console.WriteLine("Vyberte si z následující nabídky:");
+            //    Console.WriteLine();
+            //    Console.WriteLine("1. Hypotéky");
+            //    Console.WriteLine("2. Spořicí účty");
+            //    Console.WriteLine("3. Podílové fondy");
+            //    Console.WriteLine();
+            //    Console.Write("Vaše volba (1, 2 nebo 3): ");
+            //    string volba = Console.ReadLine();
+            //    Console.WriteLine();
+
+            //    if (jeKlientem == "ano")
+            //    {
+            //        telefonKlient = "+420 100 200";
+
+            //        switch (volba)
+            //        {
+            //            case "1":
+            //                telefonProdukt = "500";
+            //                Console.WriteLine("Zavolejte na číslo {0} {1}", telefonKlient, telefonProdukt);
+            //                break;
+            //            case "2":
+            //                telefonProdukt = "600";
+            //                Console.WriteLine("Zavolejte na číslo {0} {1}", telefonKlient, telefonProdukt);
+            //                break;
+            //            case "3":
+            //                telefonProdukt = "700";
+            //                Console.WriteLine("Zavolejte na číslo {0} {1}", telefonKlient, telefonProdukt);
+            //                break;
+            //            default:
+            //                Console.WriteLine("Vybrali jste neznámou volbu.");
+            //                break;
+            //        }
+            //    }
+            //    else if (jeKlientem == "ne")
+            //    {
+            //        telefonNeklient = "+420 100 300";
+
+            //        switch (volba)
+            //        {
+            //            case "1":
+            //                telefonProdukt = "500";
+            //                Console.WriteLine("Zavolejte na číslo {0} {1}", telefonNeklient, telefonProdukt);
+            //                break;
+            //            case "2":
+            //                telefonProdukt = "600";
+            //                Console.WriteLine("Zavolejte na číslo {0} {1}", telefonNeklient, telefonProdukt);
+            //                break;
+            //            case "3":
+            //                telefonProdukt = "700";
+            //                Console.WriteLine("Zavolejte na číslo {0} {1}", telefonNeklient, telefonProdukt);
+            //                break;
+            //            default:
+            //                Console.WriteLine("Vybrali jste neznámou volbu.");
+            //                break;
+            //        }
+            //    }
+            //}
+
+            //************************************************************************
+            //************************************************************************
+
+            string provestVypocet = "ano";
+            while (provestVypocet == "ano")
             {
-                Console.WriteLine();
-                Console.WriteLine("Vaše volba nebyla rozpoznána.");
-            }
-            else
-            {
-                Console.WriteLine();
-                Console.WriteLine("S jakým produktem potřebujete poradit?");
-                Console.WriteLine();
-                Console.WriteLine("Vyberte si z následující nabídky:");
-                Console.WriteLine();
-                Console.WriteLine("1. Hypotéky");
-                Console.WriteLine("2. Spořicí účty");
-                Console.WriteLine("3. Podílové fondy");
-                Console.WriteLine();
-                Console.Write("Vaše volba (1, 2 nebo 3): ");
-                string volba = Console.ReadLine();
-                Console.WriteLine();
+                Console.Write("Zadejte první číslo: ");
+                int x = Int32.Parse(Console.ReadLine());
+                Console.Write("Zadejte druhé číslo: ");
+                int y = Int32.Parse(Console.ReadLine());
+                Console.Write("Zadejte požadovanou operaci (+ - * /): ");
+                string operace = Console.ReadLine();
 
-                if (jeKlientem == "ano")
+                switch (operace)
                 {
-                    telefonKlient = "+420 100 200";
-
-                    switch (volba)
-                    {
-                        case "1":
-                            telefonProdukt = "500";
-                            Console.WriteLine("Zavolejte na číslo {0} {1}", telefonKlient, telefonProdukt);
-                            break;
-                        case "2":
-                            telefonProdukt = "600";
-                            Console.WriteLine("Zavolejte na číslo {0} {1}", telefonKlient, telefonProdukt);
-                            break;
-                        case "3":
-                            telefonProdukt = "700";
-                            Console.WriteLine("Zavolejte na číslo {0} {1}", telefonKlient, telefonProdukt);
-                            break;
-                        default:
-                            Console.WriteLine("Vybrali jste neznámou volbu.");
-                            break;
-                    }
+                    case "+":
+                        Console.WriteLine("{0} + {1} = {2}", x, y, x + y);
+                        break;
+                    case "-":
+                        Console.WriteLine("{0} - {1} = {2}", x, y, x - y);
+                        break;
+                    case "*":
+                        Console.WriteLine("{0} * {1} = {2}", x, y, x * y);
+                        break;
+                    case "/":
+                        Console.WriteLine("{0} / {1} = {2}", x, y, x / y);
+                        break;
                 }
-                else if (jeKlientem == "ne")
-                {
-                    telefonNeklient = "+420 100 300";
-
-                    switch (volba)
-                    {
-                        case "1":
-                            telefonProdukt = "500";
-                            Console.WriteLine("Zavolejte na číslo {0} {1}", telefonNeklient, telefonProdukt);
-                            break;
-                        case "2":
-                            telefonProdukt = "600";
-                            Console.WriteLine("Zavolejte na číslo {0} {1}", telefonNeklient, telefonProdukt);
-                            break;
-                        case "3":
-                            telefonProdukt = "700";
-                            Console.WriteLine("Zavolejte na číslo {0} {1}", telefonNeklient, telefonProdukt);
-                            break;
-                        default:
-                            Console.WriteLine("Vybrali jste neznámou volbu.");
-                            break;
-                    }
-                }
-
-
+                Console.Write("Chcete provést další výpočet (ano / ne): ");
+                provestVypocet = Console.ReadLine();
             }
-
             Console.ReadKey();
         }
     }
