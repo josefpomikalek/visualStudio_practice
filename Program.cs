29ab29bbf00d51485dd594a3321dcbc8922df6b0
josefpomikalek <josef_pomikalek@yahoo.com>
2025-03-15 21:51:07 +0100

    Practice some methods from the class Math.

--- Program.cs
+++ Program.cs
@@ -4,18 +4,16 @@
     {
         static void Main(string[] args)
         {
-            Console.Write("1. číslo: ");
-            int cislo1 = Int32.Parse(Console.ReadLine());
-            Console.Write("2. číslo: ");
-            int cislo2 = Int32.Parse(Console.ReadLine());
-            int soucet = Soucet(cislo1, cislo2);
-            Console.WriteLine("{0} + {1} = {2}", cislo1, cislo2, soucet);
-            Console.ReadKey();
+            Console.WriteLine("Math.Abs(-5.5) = {0}", Math.Abs(-5.5));
+            Console.WriteLine("Math.Ceiling(3.1) = {0}", Math.Ceiling(3.1));
+            Console.WriteLine("Math.Floor(3.7) = {0}", Math.Floor(3.7));
+            Console.WriteLine("Math.Pow(2, 3) = {0}", Math.Pow(2, 3));
+            Console.WriteLine("Math.Round(2.50001) = {0}", Math.Round(2.50001));
+            Console.WriteLine("Math.Sqrt(100) = {0}", Math.Sqrt(100));
+
+
         }
 
-        static int Soucet(int x, int y)
-        {
-            return x + y;
-        }
+        
     }
 }
