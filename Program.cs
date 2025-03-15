025200e71a22bd4c9038c74373b66543ea6c1349
josefpomikalek <josef_pomikalek@yahoo.com>
2025-03-15 22:34:34 +0100

    Practice some methods used for strings.

--- Program.cs
+++ Program.cs
@@ -10,10 +10,31 @@
             Console.WriteLine("Math.Pow(2, 3) = {0}", Math.Pow(2, 3));
             Console.WriteLine("Math.Round(2.50001) = {0}", Math.Round(2.50001));
             Console.WriteLine("Math.Sqrt(100) = {0}", Math.Sqrt(100));
+            Console.WriteLine();
+
+            string[] poleRetezcu = { "Jedna", "Dva", "Tři" };
+            Console.WriteLine("String.Concat(poleRetezcu): {0}", String.Concat(poleRetezcu));
+
+            Console.WriteLine("String.IsNullOrEmpty(\"\"): {0}", String.IsNullOrEmpty(""));
+            Console.WriteLine("String.IsNullOrEmpty(\" \"): {0}", String.IsNullOrEmpty(" "));
+
+            Console.WriteLine("String.IsNullOrWhiteSpace(\"\"): {0}", String.IsNullOrWhiteSpace(""));
+            Console.WriteLine("String.IsNullOrWhiteSpace(\" \"): {0}", String.IsNullOrWhiteSpace(" "));
+            Console.WriteLine();
+
+            string retezec1 = "a,b,c,d,e,f";
+            Console.WriteLine("Řetězec '{0}' obsahuje řetězec 'a': {1}", retezec1, retezec1.Contains("a"));
+            Console.WriteLine("Řetězec '{0}' obsahuje řetězec 'a,b,c': {1}", retezec1, retezec1.Contains("a,b,c"));
+            Console.WriteLine("Řetězec '{0}' obsahuje řetězec 'a,b,c ': {1}", retezec1, retezec1.Contains("a,b,c "));
+            Console.WriteLine("Řetězec '{0}' má délku {1} znaků.", retezec1, retezec1.Length);
+            Console.WriteLine("Řetězec '{0}' začíná na 'a': {1}", retezec1, retezec1.StartsWith("a"));
+            Console.WriteLine("Řetězec '{0}' začíná na 'A': {1}", retezec1, retezec1.StartsWith("A"));
+            Console.WriteLine("Řetězec '{0}' končí na 'f': {1}", retezec1, retezec1.EndsWith("f"));
+            Console.WriteLine("Řetězec '{0}' končí na 'b': {1}", retezec1, retezec1.EndsWith("b"));
 
 
         }
 
-        
+
     }
 }
