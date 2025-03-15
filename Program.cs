94ea20bc414cd298cdebf5608b8dbfbfb92f7e21
josefpomikalek <josef_pomikalek@yahoo.com>
2025-03-15 21:25:43 +0100

    Create the method with params.

--- Program.cs
+++ Program.cs
@@ -4,7 +4,18 @@
     {
         static void Main(string[] args)
         {
-            
+            Console.Write("1. číslo: ");
+            int cislo1 = Int32.Parse(Console.ReadLine());
+            Console.Write("2. číslo: ");
+            int cislo2 = Int32.Parse(Console.ReadLine());
+            int soucet = Soucet(cislo1, cislo2);
+            Console.WriteLine("{0} + {1} = {2}", cislo1, cislo2, soucet);
+            Console.ReadKey();
+        }
+
+        static int Soucet(int x, int y)
+        {
+            return x + y;
         }
     }
 }
